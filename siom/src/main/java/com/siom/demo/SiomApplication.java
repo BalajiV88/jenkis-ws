b82262fdd8a340577e1b43e853d763a2a5e770f9
Balaji Venumbaka <Balaji Venumbaka@LAPTOP-VHVQG8VL>
2020-05-09 13:00:50 +0530

    Initial

---
+++ siom/src/main/java/com/siom/demo/SiomApplication.java
@@ -0,0 +1,13 @@
+package com.siom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiomApplication.class, args);
+	}
+
+}
---
+++ siom/src/test/java/com/siom/demo/SiomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siom.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
